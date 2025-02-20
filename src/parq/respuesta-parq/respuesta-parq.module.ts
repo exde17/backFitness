545d030982aec563940a4dbdf2ec41e448ed47e0
unknown <mabg0630@gmail.com>
2025-02-19 23:17:16 -0500

    er

--- src/parq/respuesta-parq/respuesta-parq.module.ts
+++ src/parq/respuesta-parq/respuesta-parq.module.ts
@@ -3,10 +3,29 @@ import { RespuestaParqService } from './respuesta-parq.service';
 import { RespuestaParqController } from './respuesta-parq.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RespuestaParq } from './entities/respuesta-parq.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [RespuestaParqController],
   providers: [RespuestaParqService],
-  imports: [TypeOrmModule.forFeature([RespuestaParq])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([RespuestaParq]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports:[ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log(configService.get('JWT_SECRET'));
+        return{
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+        }
+        
+      },
+    }),
+  ],
 })
 export class RespuestaParqModule {}
