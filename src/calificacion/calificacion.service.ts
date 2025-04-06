1a7d3c531f0a72327b558e9321bfbf20a5bbabec
unknown <mabg0630@gmail.com>
2025-04-06 18:21:32 -0500

    fal2

--- src/calificacion/calificacion.service.ts
+++ src/calificacion/calificacion.service.ts
@@ -5,21 +5,42 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Calificacion } from './entities/calificacion.entity';
 import { User } from 'src/user/entities/user.entity';
+import { Asistencia } from 'src/asistencia/entities/asistencia.entity';
 
 @Injectable()
 export class CalificacionService {
   constructor(
     @InjectRepository(Calificacion)
     private readonly calificacionRepository: Repository<Calificacion>,
+    @InjectRepository(Asistencia)
+    private readonly asistenciaRepository: Repository<Asistencia>,
   ) {}
   async create(user: User, createCalificacionDto: CreateCalificacionDto) {
     try {
-      const calificacion = this.calificacionRepository.create({
-        ...createCalificacionDto,
-        usuario: {id:user.id},
+
+      // Actualizo la asistencia
+      const asistencia = await this.asistenciaRepository.findOne({
+        where: {
+          documento: user.datosGenerales[0].documentNumber,
+          actividad: { id: createCalificacionDto.actividad.id },
+        },
       });
-      await this.calificacionRepository.save(calificacion);
-      return 'Calificacion creada con exito';
+      if (asistencia) {
+        asistencia.calificado = true;
+        await this.asistenciaRepository.save(asistencia);
+
+        const calificacion = this.calificacionRepository.create({
+          ...createCalificacionDto,
+          usuario: {id:user.id},
+        });
+        await this.calificacionRepository.save(calificacion);
+  
+        return 'Calificacion creada con exito';
+      } else {
+        return 'No se encontro la asistencia';
+      }
+
+      
     } catch (error) {
       console.log(error);
       return 'Error al crear la calificacion';
