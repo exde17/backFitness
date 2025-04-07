8f550cda0e0b129f1b5f36fa9ce1f5222f8a7228
unknown <mabg0630@gmail.com>
2025-04-07 17:42:46 -0500

    arreglo asistencia3

--- src/actividades/actividades.service.ts
+++ src/actividades/actividades.service.ts
@@ -79,7 +79,7 @@ export class ActividadesService {
           user: {
             id: user.id
           },
-          estado: true
+          // estado: true
         },
       });
       return actividades;
