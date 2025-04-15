3ccf6a0e498e888803d498a8a11233f87c98bdc9
unknown <mabg0630@gmail.com>
2025-04-15 12:29:54 -0500

    cambio url vercel1

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['https://fitness.joperico.xyz', 'http://localhost:53736', 'https://fitness-web-app-khaki.vercel.app/'], // ⚠️ En producción, cambia "*" por el dominio de tu frontend
+    origin: ['https://fitness.joperico.xyz', 'http://localhost:53736', 'https://fitness-web-app-khaki.vercel.app'], // ⚠️ En producción, cambia "*" por el dominio de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
